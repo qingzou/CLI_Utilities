96cd8496f4d48e3f8225eb1dd433e1660a116896
qzou3 <qzou@walmartlabs.com>
2016-03-21 09:32:04 -0700

    minor fix

--- rm.js
+++ rm.js
@@ -6,27 +6,17 @@ var fs   = require('fs').promise
 var path = require('path')
 
 function* rm_r(rootPath) {
-    let stat = yield fs.stat(rootPath);
-    if (!stat.isDirectory()) {
-        let data = yield fs.unlink(rootPath, function(err) {
-             if (err) throw err;
-             console.log('successfully deleted file ' + rootPath);
-         })
-
-         console.log('come here')
-         return [data]
-    } else {
-         var rmPromises = []
-         let fileNames = yield fs.readdir(rootPath)
-         for (var f in fileNames) {
-             let filePath = path.join(rootPath, fileNames[f])
-             let promise = yield rm_r(filePath)
-             console.log('come here 2')
-             rmPromises.push(promise)
+     let fileNames = yield fs.readdir(rootPath)
+     for (var f in fileNames) {
+         let filePath = path.join(rootPath, fileNames[f])
+         let stat = yield fs.stat(filePath)
+         if(stat.isDirectory()) {
+             yield rm_r(filePath)
+             fs.rmdir(filePath)
+         } else {
+             yield fs.unlink(filePath)
          }
-         let data = yield fs.rmdir(rootPath)
-         rmPromises.push(promise)
-         console.log('come here 3')
+
     }
     //return yield Promise.all(rmPromises)
 }
@@ -34,6 +24,7 @@ function* rm_r(rootPath) {
 function* rm() {
     var file = process.argv[2]
     yield rm_r(file)
+    fs.rmdir(file)
 }
 
 module.exports = rm
